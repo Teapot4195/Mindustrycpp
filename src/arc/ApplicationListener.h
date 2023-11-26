2fbe29546d40bbc8dd3f74b351eb950c7f20eb15
Alex Huang <huangalex409@gmail.com>
2023-11-26 14:44:19 -0500

    Impl interface arc.ApplicationListener
    Broken on Fi, but compiles due to forward decl

---
+++ src/arc/ApplicationListener.h
@@ -0,0 +1,36 @@
+//
+// Created by teapot on 26/11/23.
+//
+
+#ifndef MINDUSTRY_SRC_ARC_APPLICATIONLISTENER_H
+#define MINDUSTRY_SRC_ARC_APPLICATIONLISTENER_H
+
+#include <memory>
+#include <cstdint>
+
+//Forward decl to resolve circular reference
+class Fi;
+
+class ApplicationListener {
+public:
+    virtual ~ApplicationListener() = default;
+
+    virtual void init() {}
+
+    virtual void resize(std::uint32_t width, std::uint32_t height) {}
+
+    virtual void update() {}
+
+    virtual void pause() {}
+
+    virtual void resume() {}
+
+    virtual void dispose() {}
+
+    virtual void exit() {}
+
+    virtual void fileDropped(std::shared_ptr<Fi> file) {}
+};
+
+
+#endif //MINDUSTRY_SRC_ARC_APPLICATIONLISTENER_H
