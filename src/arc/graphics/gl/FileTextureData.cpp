dab79d5feba62a2fa1690a0ca3b95abbe0d9bf68
Alex Huang <huangalex409@gmail.com>
2023-12-01 20:28:37 -0500

    Broken: impl arc.graphics.gl.FileTextureData
    broken on arc.graphics.Pixmap

---
+++ src/arc/graphics/gl/FileTextureData.cpp
@@ -0,0 +1,82 @@
+//
+// Created by teapot on 01/12/23.
+//
+
+#include "FileTextureData.h"
+
+#include <arc/files/Fi.h>
+#include <arc/graphics/Pixmap.h>
+#include <util/ArcRuntimeException.h>
+
+#include <utility>
+
+FileTextureData::FileTextureData(std::shared_ptr<Fi> file, std::shared_ptr<Pixmap> preloadedPixmap, bool useMipMaps)
+  : file(std::move(file)),
+    pixmap(std::move(preloadedPixmap)),
+    useMipMaps_(useMipMaps)
+{
+    if (!pixmap) {
+        width = pixmap->width;
+        height = pixmap->height;
+    }
+}
+
+bool FileTextureData::isPrepared() {
+    return isPrepared_;
+}
+
+void FileTextureData::preapre() {
+    if (isPrepared_)
+        throw ArcRuntimeException("Already Prepared");
+    if (pixmap) {
+        pixmap = std::make_shared<Pixmap>(file);
+        width = pixmap->width;
+        height = pixmap->height;
+    }
+    isPrepared_ = true;
+}
+
+std::shared_ptr<Pixmap> FileTextureData::consumePixmap() {
+    if (!isPrepared_)
+        throw ArcRuntimeException("Call prepare() before calling getPixmap()");
+    isPrepared_ = false;
+    auto p = pixmap;
+    pixmap.reset();
+    return p;
+}
+
+bool FileTextureData::disposePixmap() {
+    return true;
+}
+
+int FileTextureData::getWidth() {
+    return width;
+}
+
+int FileTextureData::getHeight() {
+    return height;
+}
+
+std::shared_ptr<Format> FileTextureData::getFormat() {
+    return Pixmap::Format::rgba8888;
+}
+
+bool FileTextureData::useMipMaps() {
+    return useMipMaps_;
+}
+
+std::shared_ptr<Fi> FileTextureData::getFileHandle() {
+    return file;
+}
+
+bool FileTextureData::isCustom() {
+    return false;
+}
+
+void FileTextureData::consumeCustomData(int &target) {
+    throw ArcRuntimeException("This TextureDAta implementation does not upload data itself");
+}
+
+std::string FileTextureData::toString() {
+    return file->toString();
+}
---
+++ src/arc/graphics/gl/FileTextureData.h
@@ -0,0 +1,52 @@
+//
+// Created by teapot on 01/12/23.
+//
+
+#ifndef MINDUSTRY_SRC_ARC_GRAPHICS_GL_FILETEXTUREDATA_H
+#define MINDUSTRY_SRC_ARC_GRAPHICS_GL_FILETEXTUREDATA_H
+
+#include <arc/graphics/TextureData.h>
+
+// Forward decl to avoid circular reference
+class Fi;
+class Pixmap;
+
+class FileTextureData : public TextureData {
+private:
+    std::shared_ptr<Fi> file;
+    int width = 0;
+    int height = 0;
+    std::shared_ptr<Pixmap> pixmap;
+    bool useMipMaps_;
+    bool isPrepared_ = false;
+
+public:
+    FileTextureData(std::shared_ptr<Fi> file, std::shared_ptr<Pixmap> preloadedPixmap, bool useMipMaps);
+
+    bool isPrepared() override;
+
+    void preapre() override;
+
+    std::shared_ptr<Pixmap> consumePixmap() override;
+
+    bool disposePixmap() override;
+
+    int getWidth() override;
+
+    int getHeight() override;
+
+    std::shared_ptr<Format> getFormat() override;
+
+    bool useMipMaps() override;
+
+    std::shared_ptr<Fi> getFileHandle();
+
+    bool isCustom() override;
+
+    void consumeCustomData(int &target) override;
+
+    std::string toString();
+};
+
+
+#endif //MINDUSTRY_SRC_ARC_GRAPHICS_GL_FILETEXTUREDATA_H
