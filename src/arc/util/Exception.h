9efa6b46f793fe83e8bbcc13860e6731c2155fbf
Alex Huang <huangalex409@gmail.com>
2023-12-05 13:24:15 -0500

    Stacktraces in supported exceptions!

--- src/arc/util/Exception.h
+++ src/arc/util/Exception.h
@@ -5,9 +5,81 @@
 #ifndef MINDUSTRY_SRC_ARC_UTIL_EXCEPTION_H
 #define MINDUSTRY_SRC_ARC_UTIL_EXCEPTION_H
 
+#include <iostream>
 #include <stdexcept>
 
-#define basicException(Name) class Name : public std::runtime_error {public: using std::runtime_error::runtime_error;};
+#include <boost/stacktrace.hpp>
+
+class Throwable : public std::runtime_error {
+private:
+    boost::stacktrace::stacktrace trace;
+
+public:
+    explicit Throwable(const std::string& what_arg) : std::runtime_error(what_arg) {
+        trace = boost::stacktrace::stacktrace();
+    }
+    explicit Throwable(const char* what_arg) : std::runtime_error(what_arg) {
+        trace = boost::stacktrace::stacktrace();
+    }
+    explicit Throwable(const runtime_error& other) noexcept : std::runtime_error(other) {
+        trace = boost::stacktrace::stacktrace();
+    }
+
+    [[nodiscard]] virtual std::string getStackTrace() const {
+        std::string str;
+        str += to_string(trace);
+        return str;
+    }
+};
+
+std::string unwrap_exception(const Throwable& e, int level = 0, std::string base = "");
+
+std::string unwrap_exception(const std::exception& e, int level = 0, std::string base = "") {
+    base += std::string(level, ' ');
+    base += "exception: ";
+    base += e.what();
+    base += '\n';
+    try
+    {
+        std::rethrow_if_nested(e);
+    }
+    catch (const Throwable& nestedException)
+    {
+        unwrap_exception(nestedException, level + 1, base);
+    }
+    catch (const std::exception& nestedException)
+    {
+        unwrap_exception(nestedException, level + 1, base);
+    }
+    catch (...) {}
+}
+
+std::string unwrap_exception(const Throwable& e, int level, std::string base) {
+    base += std::string(level, ' ');
+    base += "exception: ";
+    base += e.what();
+    base += " at ";
+    base += e.getStackTrace();
+    base += '\n';
+    try
+    {
+        std::rethrow_if_nested(e);
+    }
+    catch (const Throwable& nestedException)
+    {
+        unwrap_exception(nestedException, level + 1, base);
+    }
+    catch (const std::exception& nestedException)
+    {
+        unwrap_exception(nestedException, level + 1, base);
+    }
+    catch (...) {}
+}
+
+#define basicException(Name) class Name : public Throwable { \
+public:                                                      \
+    using Throwable::Throwable;                              \
+};
 
 basicException(IOException)
 
