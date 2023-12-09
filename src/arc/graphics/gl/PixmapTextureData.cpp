e8b5e220c0a80a64061a73e9478d707c5bf087e5
Alex Huang <huangalex409@gmail.com>
2023-12-08 21:30:10 -0500

    Implement arc.graphics.gl.PixmapTextureData
    Broken on arc.graphics.Pixmap

---
+++ src/arc/graphics/gl/PixmapTextureData.cpp
@@ -0,0 +1,53 @@
+//
+// Created by teapot on 08/12/23.
+//
+
+#include "PixmapTextureData.h"
+
+#include <utility>
+
+#include <arc/graphics/Pixmap.h>
+#include <arc/util/Exception.h>
+
+PixmapTextureData::PixmapTextureData(std::shared_ptr<Pixmap> pixm, bool useMipMaps, bool disposePixmap)
+    : pixmap(std::move(pixm)), uMipMaps(useMipMaps), dPixmap(disposePixmap) {}
+
+bool PixmapTextureData::disposePixmap() {
+    return dPixmap;
+}
+
+std::shared_ptr<Pixmap> PixmapTextureData::consumePixmap() {
+    return pixmap;
+}
+
+int PixmapTextureData::getWidth() {
+    return pixmap->width;
+}
+
+int PixmapTextureData::getHeight() {
+    return pixmap->height;
+}
+
+std::shared_ptr<Format> PixmapTextureData::getFormat() {
+    return Format::rgba8888;
+}
+
+bool PixmapTextureData::useMipMaps() {
+    return uMipMaps;
+}
+
+bool PixmapTextureData::isCustom() {
+    return false;
+}
+
+void PixmapTextureData::consumeCustomData(int &target) {
+    throw ArcRuntimeException("This TextureData implementation does not upload data itself");
+}
+
+bool PixmapTextureData::isPrepared() {
+    return true;
+}
+
+void PixmapTextureData::prepare() {
+    throw ArcRuntimeException("preapre() must not be called on a PixmapTextureData instance as it is already prepared");
+}
---
+++ src/arc/graphics/gl/PixmapTextureData.h
@@ -0,0 +1,43 @@
+//
+// Created by teapot on 08/12/23.
+//
+
+#ifndef MINDUSTRY_SRC_ARC_GRAPHICS_GL_PIXMAPTEXTUREDATA_H
+#define MINDUSTRY_SRC_ARC_GRAPHICS_GL_PIXMAPTEXTUREDATA_H
+
+#include <arc/graphics/TextureData.h>
+
+// Forward decl to avoid circular reference
+
+class PixmapTextureData : public TextureData {
+public:
+    const std::shared_ptr<Pixmap> pixmap;
+
+    const bool uMipMaps;
+    const bool dPixmap;
+
+    PixmapTextureData(std::shared_ptr<Pixmap> pixm, bool useMipMaps, bool disposePixmap);
+
+    bool disposePixmap() override;
+
+    std::shared_ptr<Pixmap> consumePixmap() override;
+
+    int getWidth() override;
+
+    int getHeight() override;
+
+    std::shared_ptr<Format> getFormat() override;
+
+    bool useMipMaps() override;
+
+    bool isCustom() override;
+
+    void consumeCustomData(int &target) override;
+
+    bool isPrepared() override;
+
+    void prepare() override;
+};
+
+
+#endif //MINDUSTRY_SRC_ARC_GRAPHICS_GL_PIXMAPTEXTUREDATA_H
