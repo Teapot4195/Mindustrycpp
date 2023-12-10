bcdaf6d88d5bf010412ea6e6222513ceaaca8580
Alex Huang <huangalex409@gmail.com>
2023-12-10 11:37:59 -0500

    add more types to arc.struct

--- src/arc/struct.h
+++ src/arc/struct.h
@@ -5,7 +5,9 @@
 #ifndef MINDUSTRY_SRC_ARC_STRUCT_H
 #define MINDUSTRY_SRC_ARC_STRUCT_H
 
+#include <array>
 #include <vector>
+#include <unordered_map>
 
 struct Runnable {
     virtual ~Runnable() = default;
@@ -18,5 +20,11 @@ using Seq = std::vector<T>;
 using IntSeq = Seq<int>;
 using LongSeq = Seq<long long>;
 using ULongSeq = Seq<unsigned long long>;
+
+template <typename T, std::size_t size>
+using ObjectSet = std::array<T, size>;
+
+template <typename K, typename V>
+using ObjectMap = std::unordered_map<K, V>;
 
 #endif //MINDUSTRY_SRC_ARC_STRUCT_H
