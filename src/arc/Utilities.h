472458372d8ccb97c7abebfb0ed5762c934f9389
Alex Huang <huangalex409@gmail.com>
2023-12-10 21:09:17 -0500

    Make a copy when replace all

--- src/arc/Utilities.h
+++ src/arc/Utilities.h
@@ -116,7 +116,7 @@ namespace util {
         return result;
     }
 
-    std::string& replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    std::string replaceAll(std::string str, const std::string& from, const std::string& to) {
         size_t start_pos = 0;
         while((start_pos = str.find(from, start_pos)) != std::string::npos) {
             str.replace(start_pos, from.length(), to);
